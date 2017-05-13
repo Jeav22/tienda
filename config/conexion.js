2ee4873c449821fac96fba96f7e1175c18488f07
Jeav22 <jorge.arenas@correo.usa.edu.co>
2017-05-12 19:39:28 -0500

    registro y login mysql

--- config/conexion.js
+++ config/conexion.js
@@ -1,12 +1,12 @@
 var conexion = {
     client: "mysql",
     connection: {
-        host: "172.30.10.119",
-        user: "clase",
-        password: "some_pass",
-        database: "login_db",
+        host: "sql10.freemysqlhosting.net",
+        user: "sql10174142",
+        password: "p99I3SVsM9",
+        database: "sql10174142",
         charset: "utf8"
-        }
+    }
 };
 
 module.exports.conexion = conexion;
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+    res.render('login', { title: 'Express' });
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup', { title: 'Express' });
 });
 
 router.get('/bienvenido', function(req, res, next) {
 
-if(!req.isAuthenticated()){
-  res.render('login', {title: 'Express'});
-}else{
-  var usuario = req.user;
-  if(usuario != undefined){
-    usuario = req.user.toJSON();
-  }
-  res.render('bienvenido', {tilte: "Express", usuario: usuario});
-}
+    if (!req.isAuthenticated()) {
+        res.render('login', { title: 'Express' });
+    } else {
+        var usuario = req.user;
+        if (usuario != undefined) {
+            usuario = req.user.toJSON();
+        }
+        res.render('bienvenido', { tilte: "Express", usuario: usuario });
+    }
 });
 
 module.exports = router;
